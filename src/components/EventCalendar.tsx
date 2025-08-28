1725358b02f619edf0987f4ca8fa814b48853f57
Clecio Batista Santos <cleciosan@hotmail.com>
2025-08-28 17:19:36 -0300

    Finalize implementation EventCalendar

--- src/components/EventCalendar.tsx
+++ src/components/EventCalendar.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import Image from "next/image";
 import { title } from "process";
 import { useState } from "react";
 import Calendar from "react-calendar";
@@ -40,11 +41,23 @@ const EventCalendar = () => {
             <Calendar
                 onChange={onChange}
                 value={value} />
-                <div className="flex flex-col gap-4">
-                    {events.map(event=> (
-                      <div className="" key={event.id}></div>
-                    ))}
-                </div>
+            <div className="flex items-center justify-between">
+                <h1 className="text-xl font-semibold my-4">Events</h1>
+                <Image src="/moreDark.png" alt="" width={20} height={20} />
+            </div>
+            <div className="flex flex-col gap-4">
+                {events.map(event => (
+                    <div
+                        className="p-5 rounded-md border-2 border-gray-100 border-t-4 odd:border-t-lamaSky even:border-t-lamaPurple"
+                        key={event.id}>
+                        <div className="flex items-center justify-between">
+                            <h1 className="font-semibold text-gray-600">{event.title}</h1>
+                            <span className="text-gray-300 text-xs">{event.time}</span>
+                        </div>
+                        <p className="mt-2 text-gray-400 text-sm">{event.description}</p>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
