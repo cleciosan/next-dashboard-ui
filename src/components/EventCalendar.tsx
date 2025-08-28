8a272cb22eda76764a60d4a53e29240f8101d610
Clecio Batista Santos <cleciosan@hotmail.com>
2025-08-28 16:47:08 -0300

    Created and implemented EventCalendar and alter Admin Page

--- src/components/EventCalendar.tsx
+++ src/components/EventCalendar.tsx
@@ -1,14 +1,52 @@
 "use client"
 
+import { title } from "process";
+import { useState } from "react";
+import Calendar from "react-calendar";
+import 'react-calendar/dist/Calendar.css';
+
 type ValuePiece = Date | null;
 
 type Value = ValuePiece | [ValuePiece, ValuePiece];
 
+// Temporary
+const events = [
+    {
+        id: 1,
+        title: "Lorem ipsum dolor",
+        time: "12:00 PM - 2:00 PM",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    {
+        id: 2,
+        title: "Lorem dolor ipsum",
+        time: "08:00 AM - 2:00 PM",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    {
+        id: 3,
+        title: "Ipsum Dolor Lorem",
+        time: "09:00 AM - 4:00 PM",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    }
+];
 
 const EventCalendar = () => {
-  return (
-    <div className=''>EventCalendar</div>
-  )
+    const [value, onChange] = useState<Value>(new Date());
+
+
+    return (
+        <div className='bg-white p-4 rounded-md'>
+            <Calendar
+                onChange={onChange}
+                value={value} />
+                <div className="flex flex-col gap-4">
+                    {events.map(event=> (
+                      <div className="" key={event.id}></div>
+                    ))}
+                </div>
+        </div>
+    )
 }
 
 export default EventCalendar
